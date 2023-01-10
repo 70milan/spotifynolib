8a4fb45887a48ce6a997f41653db303fe8715734
Milan Barot <7milan16@gmail.com>
2023-01-09 23:40:23 -0600

    views created. Next up, dashbboard creation

--- spotify/main/sequel.sql
+++ spotify/main/sequel.sql
@@ -1,38 +1,42 @@
 /* count liked songs by year */
 
-insert into dataeng.song_cont ("year", song_ct)
+create view master_sp.song_cont as
 select 
-distinct(right(date_added, 4)) as "year"
+distinct(left(date_added, 4)) as "year"
 ,count(track_list) as song_ct
-from dataeng.song_all_details
+from master_sp.dim_all_details
 group by "year" order by 1 desc;
 
 /* tracks by year */
 
-select min("2018") as "2018",min("2019") as "2019",min("2020") as "2020",min("2021")as "2021", min("2022") as "2022" from(
+create view master_sp.tracks_yearwise as 
+select min("2018") as "2018",min("2019") as "2019",min("2020") as "2020",min("2021")as "2021", min("2022") as "2022", min("2023") as "2023" from(
 select
-YEAR_ADDED,
-row_number() over(partition by year_added order by track_list) as rn,
-case when	YEAR_ADDED = '2018' then track_list else null end as "2018",
-case when	YEAR_ADDED = '2019' then track_list else null end as "2019",
-case when	YEAR_ADDED = '2020' then track_list else null end as "2020",
-case when	YEAR_ADDED = '2021' then track_list else null end as "2021",
-case when	YEAR_ADDED = '2022' then track_list else null end as "2022"
-from dataeng.year_added group by year_added, track_list) as temp
+date_added,
+row_number() over(partition by left(date_added, 4) order by track_list) as rn,
+case when	left(date_added, 4) = '2018' then track_list else null end as "2018",
+case when	left(date_added, 4) = '2019' then track_list else null end as "2019",
+case when	left(date_added, 4) = '2020' then track_list else null end as "2020",
+case when	left(date_added, 4) = '2021' then track_list else null end as "2021",
+case when	left(date_added, 4) = '2022' then track_list else null end as "2022",
+case when	left(date_added, 4) = '2023' then track_list else null end as "2023"
+from master_sp.dim_all_details group by date_added, track_list) as temp
 group by rn order by rn
 
 /* artists by year */
-
-select min("2018") as "2018",min("2019") as "2019",min("2020") as "2020",min("2021")as "2021", min("2022") as "2022" from(
+drop view if exists master_sp.artists_yearwise;
+create view master_sp.artists_yearwise as 
+select min("2018") as "2018",min("2019") as "2019",min("2020") as "2020",min("2021")as "2021", min("2022") as "2022",min("2023") as "2023" from(
 select
-YEAR_ADDED,
-row_number() over(partition by year_added order by artists_list) as rn,
-case when	YEAR_ADDED = '2018' then artists_list else null end as "2018",
-case when	YEAR_ADDED = '2019' then artists_list else null end as "2019",
-case when	YEAR_ADDED = '2020' then artists_list else null end as "2020",
-case when	YEAR_ADDED = '2021' then artists_list else null end as "2021",
-case when	YEAR_ADDED = '2022' then artists_list else null end as "2022"
-from dataeng.year_added group by year_added, artists_list) as temp
+date_added,
+row_number() over(partition by left(date_added, 4) order by artists_list) as rn,
+case when	left(date_added, 4) = '2018' then artists_list else null end as "2018",
+case when	left(date_added, 4) = '2019' then artists_list else null end as "2019",
+case when	left(date_added, 4) = '2020' then artists_list else null end as "2020",
+case when	left(date_added, 4) = '2021' then artists_list else null end as "2021",
+case when	left(date_added, 4) = '2022' then artists_list else null end as "2022",
+case when	left(date_added, 4) = '2023' then artists_list else null end as "2023"
+from master_sp.dim_all_details group by date_added, artists_list) as temp
 group by rn order by rn
 
 /* artists by year */
